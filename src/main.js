715b489631e2e382687d5ff886091c54be314173
xnliuxioalong <@Liu84458229>
2020-08-05 10:29:46 +0800

    增加elUI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { size: 'small', zIndex: 1000 });
 
 new Vue({
   router,
